e07664be7fda7cd23075a39c79a310756ef0795a
Berkay <berkaysutlu94@gmail.com>
2023-09-21 16:32:54 +0300

    Initial Commit

---
+++ BasicExpense/BasicExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicExpenseApp.swift
+//  BasicExpense
+//
+//  Created by Berkay Sutlu on 21.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
